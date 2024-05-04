5df32e911eacf3027f93aeb6e67e8d4e53264faa
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-05-05 00:44:53 +0800

    .

--- FPS_test.py
+++ FPS_test.py
@@ -45,7 +45,7 @@ while True:
   frame = cv2.resize(frame, (2560, 1440))
 
   (h, w) = frame.shape[:2]
-  blob = cv2.dnn.blobFromImage(cv2.resize(frame, (640, 360)), 1.0, (640, 360), (104.0, 177.0, 123.0))
+  blob = cv2.dnn.blobFromImage(cv2.resize(frame, (2560, 1440)), 1.0, (2560, 1440), (104.0, 177.0, 123.0))
   net.setInput(blob)
   detections = net.forward()
 
